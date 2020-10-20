e2477a1285211618c2fff0508bdcb02bc9fb4e86
Jeremy Hage <jhage.codingkr@gmail.com>
2020-10-20 08:38:53 -0500

    added search history limit

--- src/Components/PokemonSearch/SearchedPokemon.js
+++ src/Components/PokemonSearch/SearchedPokemon.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { usePokemonCache } from "../../Hooks/usePokemonCache";
 import "./SearchedPokemon.css";
 
 export default function SearchedPokemon({ onSelect }) {
   const [cache] = usePokemonCache();
+  const [searchHistory, setSearchHistory] = useState([]);
+
+  useEffect(() => {
+    let keys = [];
+    Object.keys(cache).forEach((key) => keys.push(key));
+    setSearchHistory([...keys]);
+  }, [cache]);
 
   return (
     <div className="searched-pokemon-header">
       <div className="searched-pokemon-wrapper">
-        {cache
-          ? Object.keys(cache).map((name) => (
+        {searchHistory.length <= 10
+          ? searchHistory.map((name) => (
               <button
                 style={{
                   minWidth: `${name.length}ch`,
@@ -23,7 +30,20 @@ export default function SearchedPokemon({ onSelect }) {
                 {name}
               </button>
             ))
-          : null}
+          : searchHistory.slice(-10).map((name) => (
+              <button
+                style={{
+                  minWidth: `${name.length}ch`,
+                  maxWidth: `${name.length + 3}ch`,
+                  margin: "5px",
+                }}
+                key={name}
+                className="searched-pokemon"
+                onClick={() => onSelect(name)}
+              >
+                {name}
+              </button>
+            ))}
       </div>
     </div>
   );
